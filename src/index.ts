2721d437fc80845cf0d65e1263a6f8abd6a03cf6
Davide Cavezza <none@none.invalid>
2023-10-23 10:40:08 +0200

    Aggiunta REST welcome

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import express from 'express'
 const app = express()
 const port = 8080
 
+app.get('/welcome', (request, response) => {
+    response.status(200).send('Benvenuto in quest\'app Express')
+})
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
