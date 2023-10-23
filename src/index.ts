89a57cc19c214a92953fa7bdf7084a0ef4673771
Davide Cavezza <none@none.invalid>
2023-10-23 10:49:50 +0200

    Aggiunto middleware JSON

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from 'express'
 const app = express()
 const port = 8080
 
+app.use(express.json())
+
 app.get('/welcome', (request, response) => {
     response.status(200).send('Benvenuto in quest\'app Express')
 })
